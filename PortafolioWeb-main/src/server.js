0ffae355108f10b5592a91bcc3f1a0f534c5cf22
Ariana Mayerli <134402331+arianacatani2009@users.noreply.github.com>
2023-12-12 15:07:55 -0500

    Add files via upload

---
+++ PortafolioWeb-main/src/server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const path = require('path');
+const { engine }  = require('express-handlebars')
+
+// Inicializaciones
+const app = express()
+
+// Configuraciones 
+app.set('port',process.env.port || 3000)
+
+//Configuraciones extras//
+
+app.set('views',path.join(__dirname, 'views'))
+app.engine('.hbs',engine({
+    defaultLayout:'main',
+    layoutsDir: path.join(app.get('views'),'layouts'),
+    partialsDir: path.join(app.get('views'),'partials'),
+    extname:'.hbs'
+}))
+app.set('view engine','.hbs')
+
+
+
+
+app.set('views',path.join(__dirname, 'views'))
+
+// Middlewares 
+//server va a trabajar con info en base a formularios
+
+app.use(express.urlencoded({extended:false}))
+
+
+// Variables globales
+
+
+// Rutas 
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+app.use(require('./routers/index.routes'))
+// Rutas Alterna
+
+// Archivos estáticos
+//Definir archivos estaticos y publicos
+app.use(express.static(path.join(__dirname,'public')))
+
+//exportar la variable app
+module.exports = app
